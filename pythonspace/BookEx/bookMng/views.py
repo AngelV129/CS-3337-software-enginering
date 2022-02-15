d4b553728ced2f165896920f85944041bad0a43c
Angel Villalobos <angelvillalobos1995@gmail.com>
2022-02-15 11:59:16 -0800

    week 3 1

--- pythonspace/BookEx/bookMng/views.py
+++ pythonspace/BookEx/bookMng/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import MainMenu
 
 # Create your views here.
 
@@ -8,4 +9,9 @@ from django.http import HttpResponse
 def index(request):
     #return HttpResponse(" { \"info\" : { \" author \" : \" xcode \"} }")
     # return render(request, "base.html")
-    return render(request, "bookMng/displaybooks.html")
+    return render(request,
+                  "bookMng/displaybooks.html",
+                    {
+                        'item_list': MainMenu.objects.all()
+                     }
+                  )
